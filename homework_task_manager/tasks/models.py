e2c1ea1d0e8dc69ecbfc8e8ca1785dfbe9dd314a
jyg_clemens@web.de <jyg_clemens@web.de>
2024-07-31 18:17:13 +0200

    Add all project files after fixing .gitignore

---
+++ homework_task_manager/tasks/models.py
@@ -0,0 +1,45 @@
+from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Task(models.Model):
+    STATUS_CHOICES = [
+        ('new', 'New'),
+        ('in_progress', 'In progress'),
+        ('pending', 'Pending'),
+        ('blocked', 'Blocked'),
+        ('done', 'Done')
+    ]
+
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    categories = models.ManyToManyField(Category, related_name='tasks')
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='new')
+    deadline = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+class SubTask(models.Model):
+    STATUS_CHOICES = [
+        ('new', 'New'),
+        ('in_progress', 'In progress'),
+        ('pending', 'Pending'),
+        ('blocked', 'Blocked'),
+        ('done', 'Done')
+    ]
+
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    task = models.ForeignKey(Task, on_delete=models.CASCADE, related_name='subtasks')
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='new')
+    deadline = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
